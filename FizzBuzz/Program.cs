ed1b521ab95c243364fb84e1268b53d5d1844a54
skgilmore <skg.gilmore@gmail.com>
2021-05-17 10:23:33 -0500

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool divisibleByThree;
+            bool divisibleByFive;
+
+            for (int i=1; i<= 100; i++ )
+
+            {
+                divisibleByThree = (i % 3 == 0);
+                divisibleByFive = (i % 5 == 0);
+
+                if (divisibleByThree && divisibleByFive)
+                
+                    Console.WriteLine("fizzbuzz");
+                
+                else if (divisibleByThree)
+                
+                    Console.WriteLine("fizz");
+                
+                else if (divisibleByFive) 
+                
+                    Console.WriteLine("buzz");
+                
+               else
+                
+                  Console.WriteLine(i);
+                
+
+            }
+            Console.Read();
+        }
+    }
+}
